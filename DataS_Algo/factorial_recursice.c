2419fb0f8ab0f7ac9e8450f79b7daf72b5c549ee
kadir emir yücel <kadiremiryucel123@gmail.com>
2025-10-22 23:15:20 +0300

    Add recursive factorial function in C
    Implement recursive factorial function and main logic.

---
+++ DataS_Algo/factorial_recursice.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int factorial(int sayi){
+if((sayi==1) || (sayi==0))
+    return 1;
+  else {
+  return n*(n-1);
+  }
+}
+int main(){
+int i,sayi;
+  print("faktoriyeli hesaplanmasını istediğiniz sayıyı giriniz ");
+  scanf("%d",&sayi);
+  print("the factorial of your number %d",factorial(sayi));
+}
