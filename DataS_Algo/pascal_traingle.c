ad8f3d22462d9e9daecb88cc0a8421702504de85
kadir emir yücel <kadiremiryucel123@gmail.com>
2025-10-28 20:10:01 +0300

    Fix function definition and update main logic

--- DataS_Algo/pascal_traingle.c
+++ DataS_Algo/pascal_traingle.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #define Pn 5
-unsigned long long int (int a,int b ){
-if(b==0)
-return a;
+unsigned long long int cnr (int a,int b ){
+if(a==0 || b==a)
+return 1;
 else
-return obeb(b,a%b);
+return cnr(a-1,b-1)+cnr(a-1,b);
 }
 
 int main(){
 int a,b;
   printf("enter the two number");
 scanf("%d %d",&a,&b);
-printf("the obeb result : ",obeb(a,b));
+printf("the obeb result : ",cnr(a,b));
 return 0;
 }
